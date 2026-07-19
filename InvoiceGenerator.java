959274e6858bb439e0d2a9edbcda4c0d81cffbb2
Shaik-Azeedh <153460338+Shaik-Azeedh@users.noreply.github.com>
2026-07-20 00:06:46 +0530

    Update InvoiceGenerator.java

--- InvoiceGenerator.java
+++ InvoiceGenerator.java
@@ -1,6 +1,6 @@
 public class InvoiceGenerator {
     public static void main(String[] args) {
-        for(int i=1;i<=10;i++){
+        for(int i=1;i<=100;i++){
         System.out.println("Welcome to SHAIK AZEEDH and Aahil ");
     }}}
   
